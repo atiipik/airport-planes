d7a95be03cdb552d9d47d58be62158461574b125
Atiipik <73227837+Atiipik@users.noreply.github.com>
2022-01-06 10:07:37 +0100

    html: Form search

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <pre>{{ openSky }}</pre>
+    <button @click="direction">Arrivées ↘️</button>
+    <button @click="direction = false">Départs ↗️</button>
+    <input class="btn-date" type="text" v-model="begin" />
+    <input class="btn-date" type="text" v-model="end" />
+    <input class="btn-airport" type="text" v-model="airport" />
+    <button @click="getFlightByDateAndAirport()">Vols ✈️</button>
+  </div>
 </template>
 
 <script>
@@ -14,12 +22,22 @@ export default {
   data() {
     return {
       openSky: null,
+      direction: true,  //* true = arrival, false = departure
+      airport: "LFPG",
+      begin: "1641339245",
+      end: "1641425645",
     };
   },
-  mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.openSky = response));
+  mounted() {},
+  methods: {
+    getFlightByDateAndAirport() {
+      const axios = require("axios");
+      axios
+        .get(
+          `https://opensky-network.org/api/flights/${this.direction ? "arrival" : "departure"}?airport=${this.airport}&begin=${this.begin}&end=${this.end}`
+        )
+        .then((response) => (this.openSky = response));
+    },
   },
 };
 </script>
